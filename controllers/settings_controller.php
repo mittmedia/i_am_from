584dfacb809037735bc5777aca8cac59fe44762a
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-26 18:05:38 +0200

    Refactored, smaller methods

--- controllers/settings_controller.php
+++ controllers/settings_controller.php
@@ -6,13 +6,31 @@ namespace IAmFrom
   {
     public function index()
     {
+      global $current_site;
       global $site;
       global $areas;
       global $content;
 
-      $site = \WpMvc\Site::find( 1 );
+      $site = \WpMvc\Site::find( $current_site->id );
 
       if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+
+        if ( isset( $_POST['site']['sitemeta']['i_am_from'] ) && trim( $_POST['site']['sitemeta']['i_am_from']['meta_value'] ) != '' ) {
+          $websafe_name = 'i_am_from_';
+          $websafe_name .= \WpMvc\ApplicationHelper::unique_identifier( $_POST['site']['sitemeta']['i_am_from']['meta_value'] ) . '_';
+          $websafe_name .= \WpMvc\ApplicationHelper::websafe_name( $_POST['site']['sitemeta']['i_am_from']['meta_value'] );
+
+          $site->sitemeta->{$websafe_name} = \WpMvc\SiteMeta::virgin();
+          $site->sitemeta->{$websafe_name}->site_id = $site->id;
+          $site->sitemeta->{$websafe_name}->meta_key = $websafe_name;
+          $site->sitemeta->{$websafe_name}->meta_value = $_POST['site']['sitemeta']['i_am_from']['meta_value'];
+
+          $site->sitemeta->{$websafe_name . '_link'} = \WpMvc\SiteMeta::virgin();
+          $site->sitemeta->{$websafe_name . '_link'}->site_id = $site->id;
+          $site->sitemeta->{$websafe_name . '_link'}->meta_key = $websafe_name . '_link';
+          $site->sitemeta->{$websafe_name . '_link'}->meta_value = $_POST['site']['sitemeta']['i_am_from_link']['meta_value'];
+        }
+
         $site->takes_post( $_POST['site'] );
 
         $site->save();
@@ -20,19 +38,33 @@ namespace IAmFrom
         static::redirect_to( "{$_SERVER['REQUEST_URI']}&i_am_from_updated=1" );
       }
 
-      $sitemeta_vars = get_object_vars( $site->sitemeta );
-
       $areas = array();
+
+      $this->get_areas_from_sitemeta( $areas, $site );
+
+      $content = array();
+
+      $this->make_form_content_from_areas( $content, $areas, $site );
+      
+      $this->make_form_content_from_new_area( $content, $site );
+
+      $this->render( $this, "index" );
+    }
+
+    private function get_areas_from_sitemeta( &$areas, $site )
+    {
+      $sitemeta_vars = get_object_vars( $site->sitemeta );
 
       foreach ( $sitemeta_vars as $key => $value ) {
         if ( preg_match( '/^((?!.*_link)i_am_from.*)$/', $key ) )
           array_push( $areas, $site->sitemeta->{$key} );
       }
+    }
 
-      $content = array();
-
+    private function make_form_content_from_areas( &$content, $areas, $site )
+    {
       foreach ( $areas as $area ) {
-        $area_content = array(
+        $content[] = array(
           'title' => __( 'Name' ),
           'name' => $site->sitemeta->{$area->meta_key}->meta_key,
           'type' => 'text',
@@ -41,7 +73,7 @@ namespace IAmFrom
           'key' => 'meta_value'
         );
 
-        $area_link = array(
+        $content[] = array(
           'title' => __( 'Link' ),
           'name' => $site->sitemeta->{$area->meta_key . '_link'}->meta_key,
           'type' => 'text',
@@ -50,12 +82,39 @@ namespace IAmFrom
           'key' => 'meta_value'
         );
 
-        array_push( $content, $area_content );
-        array_push( $content, $area_link );
-        array_push( $content, array( 'type' => 'spacer' ) );
+        $content[] = array( 'type' => 'spacer' );
       }
+    }
 
-      $this->render( $this, "index" );
+    private function make_form_content_from_new_area( &$content, &$site )
+    {
+      $site->sitemeta->i_am_from = \WpMvc\SiteMeta::virgin();
+      $site->sitemeta->i_am_from->site_id = $site->id;
+      $site->sitemeta->i_am_from->meta_key = 'i_am_from';
+      $site->sitemeta->i_am_from->meta_value = '';
+
+      $site->sitemeta->i_am_from_link = \WpMvc\SiteMeta::virgin();
+      $site->sitemeta->i_am_from_link->site_id = $site->id;
+      $site->sitemeta->i_am_from_link->meta_key = 'i_am_from_link';
+      $site->sitemeta->i_am_from_link->meta_value = '';
+
+      $content[] = array(
+        'title' => __( 'Name' ),
+        'name' => $site->sitemeta->i_am_from->meta_key,
+        'type' => 'text',
+        'object' => $site->sitemeta->i_am_from,
+        'default_value' => $site->sitemeta->i_am_from->meta_value,
+        'key' => 'meta_value'
+      );
+
+      $content[] = array(
+        'title' => __( 'Link' ),
+        'name' => $site->sitemeta->i_am_from_link->meta_key,
+        'type' => 'text',
+        'object' => $site->sitemeta->i_am_from_link,
+        'default_value' => $site->sitemeta->i_am_from_link->meta_value,
+        'key' => 'meta_value'
+      );
     }
   }
 }
