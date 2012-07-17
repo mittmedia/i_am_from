bc9fc9d0920c229a305c41c8724f1aecd38fec0c
Fredrik Sundström <fredrik.sundstrom@gmail.com>
2012-07-17 09:20:25 +0200

    namespace lang

--- views/settings/index.html.php
+++ views/settings/index.html.php
@@ -2,8 +2,8 @@
 
 <div class="wrap i_am_from">
   <div id="icon-options-general" class="icon32"><br></div>
-  <h2><?php _e( 'I Am From Settings' ); ?></h2>
-  <h3><?php _e( 'Configure Areas' ); ?></h3>
+  <h2><?php _e( 'I Am From Settings', 'i-am-from' ); ?></h2>
+  <h3><?php _e( 'Configure Areas', 'i-am-from' ); ?></h3>
   <?php
 
   \WpMvc\FormHelper::render_form( $site, $content );
